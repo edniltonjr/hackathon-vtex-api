a91527098026822e27973643f3af296282112418
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-05-04 17:16:21 -0300

    adding temp images

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,15 @@ Supplier.create(cnpj: '12312312232354', phone: '61999271632',
 # Shoes
 Product.create([name: 'Tênis Nike Air Max 270 Feminino', value: 200,
 				in_stock: true, quantity: 48, comission: 0, category_id: 3, 
-				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/tenis1.png"])
+				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/tenis1.jpg"])
 
 Product.create([name: 'Tênis Nike Air Max 270 Masculino', value: 220,
 				in_stock: true, quantity: 32, comission: 0, category_id: 3,
-				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/tenis2.png"])
+				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/tenis2.jpg"])
 
 Product.create([name: 'Tênis Nike Turbo 2018', value: 130,
 				in_stock: true, quantity: 228, comission: 0, category_id: 3,
-				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/tenis3.png"])
+				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/tenis3.jpg"])
 
 Product.create([name: 'Tênis Nike Super Model', value: 310,
 				in_stock: false, quantity: 13, comission: 25, category_id: 3, 
@@ -53,7 +53,7 @@ Product.create([name: 'Camisa Comum Alternativa', value: 60,
 # Shorts
 Product.create([name: 'Short Feminino Jeans', value: 60,
 				in_stock: true, quantity: 23, comission: 0, category_id: 2, 
-				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/shorts1.jpeg"])
+				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/shorts1.jpg"])
 
 Product.create([name: 'Shorts Masculino Praia', value: 50,
 				in_stock: false, quantity: 97, comission: 25, category_id: 2, 
@@ -62,11 +62,11 @@ Product.create([name: 'Shorts Masculino Praia', value: 50,
 # Hat
 Product.create([name: 'Chapeu Praia', value: 20,
 				in_stock: true, quantity: 72, comission: 0, category_id: 4, 
-				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/chapeu1.jpg"])
+				supplier: Supplier.first, in_market: 1, photo: "#{tmp_dir}/images/products/chapeu1.jpeg"])
 
 Product.create([name: 'Boné Aba Circular', value: 15,
 				in_stock: false, quantity: 154, comission: 25, category_id: 4, 
-				supplier: Supplier.second, in_market: 1, photo: "#{tmp_dir}/images/products/chapeu2.jpeg"])
+				supplier: Supplier.second, in_market: 1, photo: "#{tmp_dir}/images/products/chapeu2.jpg"])
 
 # Cities
 City.create([{name: 'São José dos Campos'},
